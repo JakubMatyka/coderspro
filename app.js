67915951f3a6416a14e4a0ea50e910b1c2f0a709
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-19 09:56:37 +0100

    functions and arrays literals

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var 
+// literały obiektów i tablic
+
+var car  = {
+  manufacturer: "Ford",
+  model: 'Mondeo'
+};
+
+var car1  = {
+  'manufacturer origin': "Ford",
+  model: 'Mondeo'
+};
+
+var car2;
+
+car2.manufacturer = 'Fiat';
+car2.model = '500';
+
+// literał tablic
+
+var arr = ['red', 'green', 'blue'];
+console.log(arr[1]); 
