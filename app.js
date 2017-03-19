fac2d069c2ec3ed378bbdc1364f8a7b473c712dc
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-19 14:55:36 +0100

    objects

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-// Modyfikacja this bind()
-
-function logNameAll(arg) {
-  console.log(arg + ": " + this.name);
-}
+// Objekty - działania modyfikacje
 
 var person1 = {
-  name: 'Piter'
+  name: 'Paweł'
 };
 
-var person2 = {
-  name: 'Michael'
-};
+var person2 = new Object();
 
-var name = 'Michelle';
+// dodanie [[Put]]
+person2.name = 'Beata';
+person1.age = 22;
+person2.age = 33;
 
-// tworzę funkcję powiązaną z obiektem person1
-var logNameAll1 = logNameAll.bind(person1);
-console.log(logNameAll1('person1'));
-
-var logNameAll2 = logNameAll.bind(person2, 'person2');
-console.log(logNameAll2());
-
-person2.logName = logNameAll1;
-console.log(person2.logName('person2'));
+// modyfikacji [[Set]]
+person1.name = 'Grześ';
+person2.name = 'Rafał';
