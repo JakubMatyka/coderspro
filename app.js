99883248729250334d77c6077d44f0464a0099d0
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-19 15:22:25 +0100

    delete object property

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-// Objekty - wykrywanie właściwości
+// Obiekty - usuwanie wartości
 
-// NIEEEEEEEEEEEEE!!!!!!!!!!
-/*var person = {
-  number: 0
+var person = {
+  name: 'asdas'
 };
 
-if (person.number) {
-  console.log('true');
-} else {
-  console.log('false');
-}*/
+console.log(person.name); // asdas
 
-// OK
-/*var person1 = {
-  name: 'asdasd',
-  age: 34
-};
+delete person.name;
 
-var person2 = {
-  name: 'Jduasdas',
-  age: 654
-};
-
-console.log('name' in person1); // true
-console.log('age' in person2); // true
-console.log('height' in person2); // false*/
-
-var person1 = {
-  name: 'asdasd',
-  logName: function () {
-    console.log(this.name);
-  }
-};
-
-console.log('name' in person1); // true
-console.log(person1.hasOwnProperty('name')); // true
-console.log('toString' in person1); // true
-console.log(person1.hasOwnProperty('toString')); // false
+console.log('name' in person); // false
+console.log(person.name); // undefined
