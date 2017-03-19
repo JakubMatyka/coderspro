4eab3bc35ede190979dc63a3e98d5bb704fda014
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-19 14:32:28 +0100

    this bind()

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Modyfikacja this apply()
+// Modyfikacja this bind()
 
 function logNameAll(arg) {
   console.log(arg + ": " + this.name);
@@ -14,6 +14,12 @@ var person2 = {
 
 var name = 'Michelle';
 
-logNameAll.apply(this, ['global']); // global: Miechelle
-logNameAll.apply(person1, ['person1']); // person1: Piter
-logNameAll.apply(person2, ['person2']); // person2: Michael
+// tworzę funkcję powiązaną z obiektem person1
+var logNameAll1 = logNameAll.bind(person1);
+console.log(logNameAll1('person1'));
+
+var logNameAll2 = logNameAll.bind(person2, 'person2');
+console.log(logNameAll2());
+
+person2.logName = logNameAll1;
+console.log(person2.logName('person2'));
