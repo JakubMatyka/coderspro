30d94c760e53abe071c9bedd310e2a07e5f020de
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-25 16:12:10 +0100

    accumulator function - task

--- app.js
+++ app.js
@@ -1,59 +1,15 @@
-//
+// accumulator function
 
-(function () {
-    function Person(name) {
-        this.name = name;
+function accumulator(str) {
+    var response = [];
+    for(var i = 0; i < str.length; i++) {
+        var wordish = '';
+        for(var j = 0; j < i + 1; j++) {
+            wordish += j===0 ? str[i].toUpperCase() : str[i].toLowerCase();
+        }
+        response.push(wordish);
     }
+    return response.join('-');
+}
 
-    Person.prototype.sayHello = function (name) {
-        console.log('Hello %s!', this.name)
-    };
-
-    var person1 = new Person('Ula');
-    var person2 = new Person('Maria');
-
-    console.log(person1.name, person2.name);
-
-    person1.sayHello();
-    person2.sayHello();
-
-})();
-//
-(function() {
-    function Person(name) {
-        this.name = name;
-    }
-
-    Person.prototype.arrPerson = [];
-
-    var person1 = new Person('Ula');
-    var person2 = new Person('Maria');
-
-    person1.arrPerson.push('czerwony');
-    person2.arrPerson.push('niebieski');
-
-    console.log(person1.arrPerson); // ['czerwony', 'niebieski']
-    console.log(person2.arrPerson); // ['czerwony', 'niebieski']
-})();
-
-//
-(function() {
-    function Person(name) {
-        this.name = name;
-    }
-
-   var person1 = new Person('Zosia');
-   var person2 = new Person('Tosia');
-
-   Object.freeze(person1);
-
-   Person.prototype.sayHi = function() {
-       console.log('Hi');
-   };
-
-   person1.sayHi(); // 'Hi'
-   person2.sayHi(); // 'Hi'
-
-    person1.name = 'skljdfasgd'; // nie zadziała
-    person2.name = 'lajskdha'; // zadziała
-})();
+accumulator("abcd");    // "A-Bb-Ccc-Dddd"
