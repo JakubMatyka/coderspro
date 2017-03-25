dc3b820ed3838067457cf5e5e864ee46bd7ac11e
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-25 13:53:05 +0100

    prototype methods

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
-// Kontruktory i protypy 
+//
 
-function Computer() {
-    //
-}
-
-// new zwraca wartość
-var computer1 = new Computer;
-var computer2 = new Computer;
-
-console.log(computer1.constructor === Computer); // true
-
-////
-
-function Person(name) {
-    this.name = name;
-    this.sayHello = function () {
-        console.log('dasd');
-    };
-}
-
-var person1 = new Person('Piotr');
-var person2 = new Person('Konrad');
-
-console.log(person1.name); // 'Piotr'
-
-(function()  {
-    "use strict";
-
-    function Car(name) {
-        Object.defineProperty(this, 'name', {
-            get: function () {
-                return name;
-            },
-            set: function (value) {
-                name = value;
-            }
-        });
-
-        this.brrrum = function () {
-            console.log("Bruuuuummmm");
-        }
+(function () {
+    function Person(name) {
+        this.name = name;
     }
 
-    var car1 = new Car('Szybki');
-    car1.brrrum; // 'Bruuummmmm'
+    Person.prototype.sayHello = function (name) {
+        console.log('Hello %s!', this.name)
+    };
+
+    var person1 = new Person('Ula');
+    var person2 = new Person('Maria');
+
+    console.log(person1.name, person2.name);
+
+    person1.sayHello();
+    person2.sayHello();
+
+})();
+//
+(function() {
+    function Person(name) {
+        this.name = name;
+    }
+
+    Person.prototype.arrPerson = [];
+
+    var person1 = new Person('Ula');
+    var person2 = new Person('Maria');
+
+    person1.arrPerson.push('czerwony');
+    person2.arrPerson.push('niebieski');
+
+    console.log(person1.arrPerson); // ['czerwony', 'niebieski']
+    console.log(person2.arrPerson); // ['czerwony', 'niebieski']
+})();
+
+//
+(function() {
+    function Person(name) {
+        this.name = name;
+    }
+
+    // definiowanie wielu metod na raz w prototypie
+    Person.prototype = {
+        sayHello: function () {
+            // asdadsas
+        },
+        toString: function () {
+            //
+        }
+    }
 })();
